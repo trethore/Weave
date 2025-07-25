00db78a0a6600346227de3272dd311ce2f7b9630
trethore <titou.rethore@gmail.com>
2025-07-25 18:29:35 +0200

    Batman i think !

---
+++ src/client/java/tytoo/weave/WeaveClient.java
@@ -0,0 +1,10 @@
+package tytoo.weave;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeaveClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
