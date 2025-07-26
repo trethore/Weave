267336dc95bdad46be2b24f87f16f3e08c137e00
trethore <titou.rethore@gmail.com>
2025-07-26 17:07:48 +0200

    Fixed stackoverflow error !

--- src/client/java/tytoo/weave/component/components/Window.java
+++ src/client/java/tytoo/weave/component/components/Window.java
@@ -2,17 +2,33 @@ package tytoo.weave.component.components;
 
 import net.minecraft.client.gui.DrawContext;
 import tytoo.weave.component.Component;
+import tytoo.weave.utils.McUtils;
 
 public class Window extends Component<Window> {
     public Window() {
         this.parent = this;
     }
 
+
     @Override
     public void draw(DrawContext context) {
         for (Component<?> child : children) {
             child.draw(context);
         }
+    }
+
+    @Override
+    public float getLeft() {
+        return McUtils.getMc()
+                .map(mc -> (mc.getWindow().getScaledWidth() - getWidth()) / 2f)
+                .orElse(0f);
+    }
+
+    @Override
+    public float getTop() {
+        return McUtils.getMc()
+                .map(mc -> (mc.getWindow().getScaledHeight() - getHeight()) / 2f)
+                .orElse(0f);
     }
 
     @Override
