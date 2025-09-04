c4c681154a77e348b411d1a8730717ceafed672a
trethore <titou.rethore@gmail.com>
2025-09-04 17:20:00 +0200

    Created the core, so u can easily install the lib in ur mod !

--- src/client/java/tytoo/weave/WeaveClient.java
+++ src/client/java/tytoo/weave/WeaveClient.java
@@ -2,13 +2,8 @@ package tytoo.weave;
 
 import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
-import tytoo.weave.command.WeaveCommands;
-import tytoo.weave.ui.CursorManager;
-import tytoo.weave.utils.FontManager;
+
 
 public class WeaveClient implements ClientModInitializer {
     public static final Logger LOGGER = LogUtils.getLogger();
@@ -16,14 +11,7 @@ public class WeaveClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
-            ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> WeaveCommands.register(dispatcher));
-        }
-
-        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
-            FontManager.closeAll();
-            CursorManager.destroy();
-        });
+        WeaveCore.init();
         LOGGER.info("Weave initialized!");
     }
 }
---
+++ src/client/java/tytoo/weave/WeaveCore.java
@@ -0,0 +1,50 @@
+package tytoo.weave;
+
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import tytoo.weave.command.WeaveCommands;
+import tytoo.weave.ui.CursorManager;
+import tytoo.weave.utils.FontManager;
+
+
+/*
+ * Core class for initializing Weave.
+ */
+public final class WeaveCore {
+
+    private WeaveCore() {
+    }
+
+    /**
+     * Initializes Weave by registering debug commands and close event handlers.
+     * Call this method in your mod's client initializer.
+     */
+
+    public static void init() {
+        registerDebugCommands();
+        registerCloseEvent();
+    }
+
+    /**
+     * Registers debug commands if the environment is a development environment.
+     * /weave testgui
+     * /weave reloadtheme
+     */
+    private static void registerDebugCommands() {
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> WeaveCommands.register(dispatcher));
+        }
+    }
+
+    /**
+     * Registers an event handler to clean up resources when the client is stopping.
+     * This includes closing all fonts and destroying the cursor manager.
+     */
+    private static void registerCloseEvent() {
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+            FontManager.closeAll();
+            CursorManager.destroy();
+        });
+    }
+}
